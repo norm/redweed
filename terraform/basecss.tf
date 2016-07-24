56dba5c7be4d842413ff42d01b6da0144f935679
Mark Norman Francis <norm@cackhanded.net>
2016-07-24 06:58:34 +0100

    Serve example.basecss.com from S3

--- terraform/basecss.tf
+++ terraform/basecss.tf
@@ -24,6 +24,28 @@ POLICY
     }
 }
 
+resource "aws_s3_bucket" "example_basecss_bucket" {
+    bucket = "example.basecss.com"
+    acl = "public-read"
+    policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Sid": "PublicReadForGetBucketObjects",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::example.basecss.com/*"
+    }]
+}
+POLICY
+
+    website = {
+        index_document = "index.html"
+        error_document = "404.html"
+    }
+}
+
 resource "aws_route53_record" "apex_basecss" {
     name = "basecss.com"
     type = "A"
@@ -31,6 +53,19 @@ resource "aws_route53_record" "apex_basecss" {
     alias {
         name = "${aws_s3_bucket.basecss_bucket.website_domain}"
         zone_id = "${aws_s3_bucket.basecss_bucket.hosted_zone_id}"
+        evaluate_target_health = false
+    }
+
+    zone_id = "${aws_route53_zone.basecss.zone_id}"
+}
+
+resource "aws_route53_record" "example_basecss" {
+    name = "example.basecss.com"
+    type = "A"
+
+    alias {
+        name = "${aws_s3_bucket.example_basecss_bucket.website_domain}"
+        zone_id = "${aws_s3_bucket.example_basecss_bucket.hosted_zone_id}"
         evaluate_target_health = false
     }
 
